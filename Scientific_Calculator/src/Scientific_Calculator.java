99d8e0ce7583e43060e78d8cda7dd38df3b5847b
arkasnow <rainymountain@gmail.com>
2015-08-01 09:50:42 -0700

    add calculator parts which is partly working.

--- Scientific_Calculator/src/Scientific_Calculator.java
+++ Scientific_Calculator/src/Scientific_Calculator.java
@@ -13,7 +13,7 @@ public class Scientific_Calculator extends javax.swing.JFrame {
     // Constant values
     private final int BUTTON_WIDTH  = 65;
     private final int BUTTON_HEIGHT = 30;
-    private final int BUTTON_PER_LINE = 8;
+    private final int BUTTON_PER_LINE = 7;
 
     /**
      * Creates new form Scientific_Calculator
@@ -47,11 +47,11 @@ public class Scientific_Calculator extends javax.swing.JFrame {
 
         // Buttons
         // Refer to http://docs.oracle.com/javase/7/docs/api/java/lang/Math.html
-        String [] buttonsText = {"(", ")", "log", "ln", "back", "CE", "C", "/",
-                                 "abs", "neg", "exp", "", "7", "8", "9", "*",
-                                 "sin", "csc", "sqrt", "cbrt", "4", "5", "6", "-",
-                                 "cos", "sec", "", "", "1", "2", "3", "+",
-                                 "tan", "cot", "", "", "0", ".", "%", "="};
+        String [] buttonsText = {"(", ")", "abs", "", "CE", "C", "/",
+                                 "", "exp", "log", "7", "8", "9", "*",
+                                 "sin", "csc", "", "4", "5", "6", "-",
+                                 "cos", "sec", "sqrt", "1", "2", "3", "+",
+                                 "tan", "cot", "cbrt", "0", ".", "%", "="};
         // init all buttons
         this.jButtons = new javax.swing.JButton[buttonsText.length];
 
@@ -88,7 +88,6 @@ public class Scientific_Calculator extends javax.swing.JFrame {
         javax.swing.GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup()
             .addComponent(jTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE);
 
-        
         javax.swing.GroupLayout.SequentialGroup hTmpGroup = null;
         javax.swing.GroupLayout.ParallelGroup   vTmpGroup = null;
         for(int i=0; i<buttonsText.length; i++) {
@@ -123,10 +122,11 @@ public class Scientific_Calculator extends javax.swing.JFrame {
     private void jButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonActionPerformed
         // TODO add your handling code here:
         System.out.println(evt.getActionCommand());
-        calculator.addInput(evt.getActionCommand());
-        jTextField.setText(jTextField.getText() + evt.getActionCommand());
-        //jTextField.append(evt.getActionCommand());
+        String str = new String(evt.getActionCommand());
+        calculator.addInput(str);
+        jTextField.setText(calculator.getOutput());
     }
+
     private void jTextFieldKeyReleased(java.awt.event.KeyEvent evt) {                                       
         // TODO add your handling code here:
         char ch = evt.getKeyChar();
@@ -140,19 +140,23 @@ public class Scientific_Calculator extends javax.swing.JFrame {
         }
         
          // replace '\n' to '='
-        if (ch == '\n') { ch = '='; }
+        if (ch == '\n' || ch == '=') {
+            ch = '=';
+        }
         
         if (Character.isDigit(ch) || ch == '.' ||
                 ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' ||
-                ch == '=' ||
-                ch == '(' || ch == ')') {
-            calculator.addInput(String.valueOf(ch));
-            jTextField.setText(jTextField.getText() + String.valueOf(ch));
+                ch == '(' || ch == ')' || ch == '=') {
+
+            calculator.addInput(ch);
+            //jTextField.setText(jTextField.getText() + String.valueOf(ch));
+            jTextField.setText(calculator.getOutput());
             System.out.println(String.format("Key Pressed: %c", ch));
         } else {
             // ignore other inputs
         }
     }
+
     private static boolean jvmSupported(){
         String version = System.getProperty("java.version");
         if (version.startsWith("1.7") || version.startsWith("1.8")) {
@@ -162,12 +166,40 @@ public class Scientific_Calculator extends javax.swing.JFrame {
             return false;
         }
     }
+    private static boolean testCalculator() {
+        Calculator c = new Calculator();
+        c.addInput('1');
+        c.addInput('+');
+        c.addInput('1');
+        c.addInput('=');
+        System.out.println(c.getOutput());
+
+        c.addInput('1');
+        c.addInput('=');
+        System.out.println(c.getOutput());
+        
+        c.addInput("sin");
+        c.addInput('1');
+        c.addInput('=');
+        System.out.println(c.getOutput());
+        
+        c.addInput('1');
+        c.addInput('+');
+        c.addInput("C");
+        c.addInput('1');
+        c.addInput('=');
+        System.out.println(c.getOutput());
+
+        return false;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         // check the version of JVM, lower than 1.7 not supported.
         if (!jvmSupported()) {return;}
+        
+        //if (!testCalculator()) {return;}
 
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -199,23 +231,73 @@ class Calculator {
     // private parameters
     private javax.script.ScriptEngineManager manager = new javax.script.ScriptEngineManager();
     private javax.script.ScriptEngine engine = manager.getEngineByName("js");
+    private java.util.ArrayList<String> list = null;
+    private String ans = null;
+    private int open_bracket = 0;
 
-    public Calculator(){}
+    public Calculator(){
+        list = new java.util.ArrayList<>();
+    }
 
-    public void addInput(String c) {
-        ;
+    public void addInput(char c) {
+        addInput(String.valueOf(c));
     }
-    public String getInput() {
-        return "";
+    public void addInput(String s) {
+        if (s.equals("=")) {
+            //parseInput();
+            System.out.println(ans);
+            if(!list.isEmpty()) {
+                ans = calculate(ans);
+                return;
+            }
+        } else if (s.equals("C")) {
+            this.reset();
+        } else if (s.equals("CE")) {
+            list.set(list.size()-1, "");
+        } else if (s.equals("sin") || s.equals("csc") ||
+                   s.equals("cos") || s.equals("sec") ||
+                   s.equals("log") || s.equals("ln") ||
+                   s.equals("tan") || s.equals("cot") ||
+                   s.equals("sqrt") || s.equals("cbrt")||
+                   s.equals("exp") || s.equals("abs")
+                   ) {
+            list.add("Math."+s+"(");
+            open_bracket++;
+        } else {
+            list.add(s);
+        }
+        parseInput();
     }
-    public String getOutput() {
+    
+    private void parseInput() {
+        String str = "";
+        for(int i = 0; i<list.size(); i++) {
+            str += list.get(i);
+        }
+        for(int i = 0; i<open_bracket; i++) {
+            str += ")";
+        }
+        ans = str;
+    }
+    private void reset() {
+        open_bracket = 0;
+        list.clear();
+    }
+    private String calculate(String str) {
         try {
-            Object result = engine.eval("Math.sin(3+4)");
+            Object result = engine.eval(str);
             System.out.println(result.toString());
+            reset();
+            list.add(result.toString());
             return result.toString();
         } catch (javax.script.ScriptException se) {
             System.out.println(se.toString());
+            reset();
             return se.toString();
         }
+    }
+
+    public String getOutput() {
+        return ans;
     }
 }
